c3ac46dd6c000d04157ce436f7c3365cd4df6773
Erick Christian <erickchristian48@gmail.com>
2024-02-16 20:29:19 +0700

    fix: webpack throwing error
    it says cant arccess variable before declaration even though its just a kind of cyclic export/import

--- src/remotion/lib/constants.ts
+++ src/remotion/lib/constants.ts
@@ -1,7 +1,6 @@
 import { SUBTITLES } from "../assets/subtitles";
-import { msToSeconds } from "./time";
 
 const DURATION_IN_MS = SUBTITLES.at(-1)?.to ?? 0;
 
 export const FPS = 30;
-export const DURATION_IN_FRAMES = (msToSeconds(DURATION_IN_MS) + 1) * FPS;
+export const DURATION_IN_FRAMES = (DURATION_IN_MS / 1000 + 1) * FPS;
