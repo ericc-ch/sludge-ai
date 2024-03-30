68381ed78d73d21e51eeb9a0994d6966d5802c58
Erick Christian <erickchristian48@gmail.com>
2024-03-31 05:42:13 +0700

    feat: init bg videos

--- src/server/lib/init.ts
+++ src/server/lib/init.ts
@@ -1,8 +1,8 @@
 import fs from "node:fs";
 import fsPromises from "node:fs/promises";
-import { FOLDER_DOWNLOAD, FOLDER_PUBLIC } from "../paths";
+import { FOLDER_BG_VIDEOS, FOLDER_DOWNLOAD, FOLDER_PUBLIC } from "../paths";
 
-const REQUIRED_FOLDERS = [FOLDER_PUBLIC, FOLDER_DOWNLOAD];
+const REQUIRED_FOLDERS = [FOLDER_PUBLIC, FOLDER_DOWNLOAD, FOLDER_BG_VIDEOS];
 
 export const initializeFolders = async () => {
   for (const folder of REQUIRED_FOLDERS) {
